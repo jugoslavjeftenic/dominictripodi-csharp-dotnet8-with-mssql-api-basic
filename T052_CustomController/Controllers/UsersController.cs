f6da1ba0a54b553f2a5305ffccfcf2b030004575
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-18 13:27:46 +0100

    T052_CustomController

---
+++ T052_CustomController/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace T052_CustomController.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class UsersController : ControllerBase
+	{
+		public UsersController()
+		{
+
+		}
+
+		[HttpGet("GetUsers/{testValue}")]
+		public string[] GetUsers(string testValue)
+		{
+			string[] responseArray = ["test1", "test2", testValue];
+			return responseArray;
+		}
+	}
+}
