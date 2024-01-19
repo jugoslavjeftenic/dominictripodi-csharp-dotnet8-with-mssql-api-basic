44947da95aadcc0997e30ff1c9e76b1d5b3116fe
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-20 00:12:56 +0100

    T060_UsersController Put & Post

--- T060_UsersController/Controllers/UsersController.cs
+++ T060_UsersController/Controllers/UsersController.cs
@@ -48,5 +48,52 @@ namespace T060_UsersController.Controllers
 
 			return user;
 		}
+
+		[HttpPut("EditUser")]
+		public IActionResult EditUser(UserModel user)
+		{
+			string sql = @$"
+			UPDATE [TutorialAppSchema].[Users] SET
+				[FirstName] = '{user.FirstName}',
+				[LastName] = '{user.LastName}',
+				[Email] = '{user.Email}',
+				[Gender] = '{user.Gender}',
+				[Active] = '{user.Active}'
+			WHERE UserId = {user.UserId}
+			";
+            
+            if (_dapper.ExecuteSql(sql))
+			{
+				return Ok();
+			}
+
+			throw new Exception("Failed to Update user");
+		}
+
+		[HttpPost("AddUser")]
+		public IActionResult AddUser(UserModel user)
+		{
+			string sql = @$"
+			INSERT INTO [TutorialAppSchema].[Users] (
+				[FirstName],
+				[LastName],
+				[Email],
+				[Gender],
+				[Active]
+			) VALUES (
+				'{user.FirstName}',
+				'{user.LastName}',
+				'{user.Email}',
+				'{user.Gender}',
+				'{user.Active}'
+			)";
+			Console.WriteLine(sql);
+			if (_dapper.ExecuteSql(sql))
+			{
+				return Ok();
+			}
+
+			throw new Exception("Failed to Add user");
+		}
 	}
 }
