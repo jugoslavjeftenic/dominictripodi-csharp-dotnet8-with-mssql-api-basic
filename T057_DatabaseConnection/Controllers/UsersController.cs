2bb6ca388fd9ded54728b094b6d656d6db78c80c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-19 09:22:08 +0100

    T057_DatabaseConnection

---
+++ T057_DatabaseConnection/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using T057_DatabaseConnection.Data;
+
+namespace T057_DatabaseConnection.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class UsersController(IConfiguration config) : ControllerBase
+	{
+		private readonly DataContextDapper _dapper = new(config);
+
+		[HttpGet("TestConnection")]
+		public DateTime TestConnection()
+		{
+			return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+		}
+		public string[] GetUsers(string testValue)
+		{
+			string[] responseArray = ["test1", "test2", testValue];
+			return responseArray;
+		}
+	}
+}
