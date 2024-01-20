3b3a3252b61736b1311e1c3d23d3ea764eecba6b
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-20 12:45:15 +0100

    T065_EntityFramework UsersController

---
+++ T065_EntityFramework/Controllers/UsersController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using T065_EntityFramework.Data;
+using T065_EntityFramework.Dtos;
+using T065_EntityFramework.Models;
+
+namespace T065_EntityFramework.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class UsersController(IConfiguration config) : ControllerBase
+	{
+		private readonly DataContextEF _ef = new(config);
+
+		[HttpGet("GetUsers")]
+		public IEnumerable<UserModel> GetUsers()
+		{
+			return _ef.Users.ToList<UserModel>();
+		}
+
+		[HttpGet("GetUser/{userId}")]
+		public UserModel GetUser(int userId)
+		{
+			return _ef.Users
+				.Where(u => u.UserId == userId)
+				.FirstOrDefault<UserModel>() ?? throw new Exception("Failed to Get User.");
+		}
+
+		[HttpPut("EditUser")]
+		public IActionResult EditUser(UserModel user)
+		{
+			UserModel userToEdit = _ef.Users
+				.Where(u => u.UserId == user.UserId)
+				.FirstOrDefault<UserModel>() ?? throw new Exception("Failed to Get User during Update action.");
+
+			userToEdit.FirstName = user.FirstName;
+			userToEdit.LastName = user.LastName;
+			userToEdit.Email = user.Email;
+			userToEdit.Gender = user.Gender;
+			userToEdit.Active = user.Active;
+
+			if (_ef.SaveChanges() > 0)
+			{
+				return Ok();
+			}
+
+			throw new Exception("Failed to Update User.");
+		}
+
+		[HttpPost("AddUser")]
+		public IActionResult AddUser(UserToAddDto user)
+		{
+			UserModel userToAdd = new()
+			{
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				Email = user.Email,
+				Gender = user.Gender,
+				Active = user.Active
+			};
+
+			_ef.Add(userToAdd);
+			if (_ef.SaveChanges() > 0)
+			{
+				return Ok();
+			}
+
+			throw new Exception("Failed to Add User.");
+		}
+
+		[HttpDelete("DeleteUser/{userId}")]
+		public IActionResult DeleteUser(int userId)
+		{
+			UserModel userToDelete = _ef.Users
+				.Where(u => u.UserId == userId)
+				.FirstOrDefault<UserModel>() ?? throw new Exception("Failed to Get User during Delete action.");
+
+			_ef.Users.Remove(userToDelete);
+			if (_ef.SaveChanges() > 0)
+			{
+				return Ok();
+			}
+
+			throw new Exception("Failed to Delete User.");
+		}
+	}
+}
