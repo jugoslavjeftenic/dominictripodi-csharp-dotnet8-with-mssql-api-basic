5dde75a9c50eac6f3651f10cc1023ef77fa53631
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-24 09:17:00 +0100

    The option to make fields nullable has been added.

--- T070_AssignmentEntityFramework/Models/UserSalaryModel.cs
+++ T070_AssignmentEntityFramework/Models/UserSalaryModel.cs
@@ -3,7 +3,7 @@
 	public class UserSalaryModel
 	{
 		public int UserId { get; set; }
-		public decimal Salary { get; set; }
-		public decimal AvgSalary { get; set; }
+		public decimal? Salary { get; set; }
+		public decimal? AvgSalary { get; set; }
 	}
 }
